c26b149fccd21e8afde133ca46c8e167d44dce3d
Lattapan <noey.rachata@gmail.com>
2023-06-02 22:55:41 +0700

    Setup project

--- app/posts/error.js
+++
@@ -1,9 +0,0 @@
-const error = () => {
-    return (
-        <div>
-            <p>Where error page can be written</p>
-        </div>
-    );
-};
-
-export default error;
--- app/posts/loading.js
+++
@@ -1,6 +0,0 @@
-const loading = () => {
-    //can add any UI inside Loading, including Skeleton.
-    return <div>loading</div>;
-};
-
-export default loading;
